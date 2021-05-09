105395640d386983c3e167cabfa204321bb28d30
Eduardo Andrés Castillo Perera <eduardoandrescp@outlook.com>
2021-05-08 23:20:09 -0500

    Add firebase support

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,63 @@
 <script lang="ts">
-	export let name: string;
+
+	const url = "https://pacwave-leaderboard-dotnet.azurewebsites.net/api/hs";
+
+	// The data from the store
+	interface Score {
+		score: number;
+		playerName: string;
+		timestamp: string;
+	}
+
+	let scores: Score[] = [];
+
+	function getData() {
+		// Make the fetch
+		fetch(url)
+			.then(res => res.json())
+			.then((data: Score[]) => scores = data)
+			.catch(() => {
+				alert("Sorry, could not retrieve the scores");
+			});
+	}
+
+
+	getData();
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+<main class="container">
+	<div class="px-4 py-5 my-5 text-center">
+		<img class="d-block mx-auto mb-4" src="https://getbootstrap.com/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
+		<h1 class="display-5 fw-bold">High Score</h1>
+		<div class="col-lg-6 mx-auto">
+		  <p class="lead mb-4">Here are the high scores</p>
+		  <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+			<button type="button" class="btn btn-primary btn-lg px-4 me-sm-3" on:click={getData}>Update</button>
+		  </div>
+		</div>
+	  </div>
+
+	  <table class="table table-dark table-striped">
+		<thead>
+	  <tr>
+		<th scope="col">#</th>
+		<th scope="col">Name</th>
+		<th scope="col">Score</th>
+		<th scope="col">Date</th>
+	  </tr>
+	</thead>
+	<tbody>
+	  {#each scores as row, i}
+		  <tr>
+			<th scope="row">{i+1}</th>
+			<td> {row.playerName} </td>
+			<td> {row.score} </td>
+			<td> {new Date(row.timestamp)} </td>
+		  </tr>
+	  {/each}
+
+	</tbody>
+  
+	</table>
+
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
